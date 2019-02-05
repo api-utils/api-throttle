83e8d13f030bc1bc132e664f0022b98ac66a7e3d
yan_h <yan_h@hotmail.com>
2019-02-05 22:58:38 +0800

    [api-throttle]add BucketPolicy#check and BucketPolicy#assignToken

--- src/main/java/com/nobodyhub/transcendence/api/throttle/domain/BucketPolicy.java
+++ src/main/java/com/nobodyhub/transcendence/api/throttle/domain/BucketPolicy.java
@@ -1,24 +1,46 @@
 package com.nobodyhub.transcendence.api.throttle.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class BucketPolicy {
     /**
-     * The window size in second to be measured.
-     * -1 means infinite window size will be applied
+     * The window size in second to be measured. non-zero, positive
+     * Long.MAX_VALUE means infinite window size will be applied
      */
-    private int window;
+    private long window = Long.MAX_VALUE;
     /**
-     * Number of tokens that are available for {@link #window}.
-     * -1 means no limitation on the number of the tokens
+     * Number of tokens that are available for {@link #window}. non-zero, positive
+     * Long.MAX_VALUE means no limitation on the number of the tokens
      */
-    private int nToken;
+    private long nToken = Long.MAX_VALUE;
     /**
-     * Minimum interval between requests
+     * Minimum interval between requests, non-negative
      * 0 means no interval required between requests
      */
-    private int interval;
+    private long interval = 0;
+
+    public BucketPolicy(Long window, Long nToken, Long interval) {
+        if (window != null) {
+            this.window = window < 0 ? 0 : window;
+        }
+        if (nToken != null) {
+            this.nToken = nToken < 0 ? 0 : nToken;
+
+        }
+        if (interval != null) {
+            this.interval = interval < 0 ? 0 : interval;
+        }
+    }
+
+    public boolean check(Long timestamp, BucketStatus status) {
+        boolean checkInterval = interval == 0 || timestamp - status.getLastRequest() > interval;
+        boolean checkWindow = window == -1 || nToken != -1 || status.getNWindowed() < nToken;
+        boolean checkToken = status.getNToken() > 0;
+        return checkInterval && checkWindow && checkToken;
+    }
+
+    public long assignToken(long timeCollapse, BucketStatus status) {
+        return timeCollapse * nToken / window;
+    }
 }
