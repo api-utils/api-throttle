b4fafea4569ddec3ffaf833ae4ef869d0e7ae3ab
yan_h <yan_h@hotmail.com>
2019-02-13 12:31:23 +0800

    [api-throttle]Update BlockPolicy to handle status update

--- src/main/java/com/nobodyhub/transcendence/api/throttle/core/anno/BlockPolicy.java
+++ src/main/java/com/nobodyhub/transcendence/api/throttle/core/anno/BlockPolicy.java
@@ -1,5 +1,9 @@
 package com.nobodyhub.transcendence.api.throttle.core.anno;
 
+import com.nobodyhub.transcendence.api.throttle.bucket.service.ThrottleBucketService;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Policy to follow when the execution is blocked
  */
@@ -17,4 +21,77 @@ public enum BlockPolicy {
      */
     RETRY,
     ;
+
+    /**
+     * Apply the BlockPolicy to bucket status updates
+     *
+     * @param bucketService
+     * @param speedLimited
+     * @return true if OK to proceed status update
+     * @throws InterruptedException
+     */
+    public boolean apply(ThrottleBucketService bucketService, SpeedLimited speedLimited) throws InterruptedException {
+        switch (this) {
+            case WAIT: {
+                return applyWait(bucketService, speedLimited);
+            }
+            case SKIP: {
+                return applySkip(bucketService, speedLimited);
+            }
+            case RETRY: {
+                return applyRetry(bucketService, speedLimited);
+            }
+            default: {
+                throw new UnhandledBlockPolicyException(this);
+            }
+        }
+    }
+
+    /**
+     * @see BlockPolicy#RETRY
+     */
+    private boolean applyRetry(ThrottleBucketService bucketService, SpeedLimited speedLimited) throws InterruptedException {
+        String[] buckets = speedLimited.buckets();
+        int retry = speedLimited.retry();
+        long delay = speedLimited.retryDelay();
+        boolean approved = false;
+        while (!approved && --retry >= 0) {
+            approved = bucketService.checkBucket(buckets);
+            if (!approved && delay > 0) {
+                Thread.sleep(delay);
+            }
+        }
+        return approved;
+    }
+
+    /**
+     * @see BlockPolicy#SKIP
+     */
+    private boolean applySkip(ThrottleBucketService bucketService, SpeedLimited speedLimited) {
+        String[] buckets = speedLimited.buckets();
+        return bucketService.checkBucket(buckets);
+    }
+
+    /**
+     * @see BlockPolicy#WAIT
+     */
+    private boolean applyWait(ThrottleBucketService bucketService, SpeedLimited speedLimited) {
+        long nano = TimeUnit.NANOSECONDS.convert(speedLimited.waitTimeout(), TimeUnit.MILLISECONDS);
+        String[] buckets = speedLimited.buckets();
+        long start = System.nanoTime();
+        boolean approved = false;
+        while (!approved && (System.nanoTime() - start <= nano)) {
+            approved = bucketService.checkBucket(buckets);
+        }
+        return approved;
+    }
+}
+
+/**
+ * Exception when no handler found for the block policy
+ */
+class UnhandledBlockPolicyException extends RuntimeException {
+    UnhandledBlockPolicyException(BlockPolicy blockPolicy) {
+        super(String.format("No rule found to handle BlockPolicy[%s]!", blockPolicy));
+    }
 }
